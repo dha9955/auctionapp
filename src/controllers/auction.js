57133657fa6317342b340e45a48a7c6adb9095e3
1607dha <1607dha@gmail.com>
2020-12-26 01:44:12 +0700

    push

--- src/controllers/auction.js
+++ src/controllers/auction.js
@@ -1,7 +1,7 @@
 const e = require("express");
 const Auction = require("../models/auction");
 const Product = require("../models/product");
-const User = require("../models/user")
+const User = require("../models/user");
 
 exports.createAuction = (req, res) => {
   Product.findOne({ _id: req.body.product }).exec((error, product) => {
@@ -17,7 +17,7 @@ exports.createAuction = (req, res) => {
         if (error) return res.status(400).json({ error });
         if (auction) {
           product.currentPrice = auction.price;
-          product.auction.push(auction._id)
+          product.auction.push(auction._id);
           product.save();
           return res.status(201).json({ auction });
         }
@@ -29,34 +29,54 @@ exports.createAuction = (req, res) => {
 exports.getAuctionbyProduct = (req, res) => {
   const { productId } = req.params;
   if (productId) {
-    Auction.find({ product: productId }).populate({path:"user",select: "_id username"}).exec((error, auctions) => {
-      if (error) return res.status(400).json({ error });
-      if (auctions) {
-        return res.status(200).json({ auctions });
-      }
-    });
+    Auction.find({ product: productId })
+      .populate({ path: "user", select: "_id username" })
+      .exec((error, auctions) => {
+        if (error) return res.status(400).json({ error });
+        if (auctions) {
+          return res.status(200).json({ auctions });
+        }
+      });
   } else {
     return res.status(400).json({ error: "Params required" });
   }
-}
-
+};
 
 exports.getAuctionSuccessfullbyUser = (req, res) => {
-  const {userId} = req.params;
-  Auction.find({user: userId, status:1}).populate({path:"product", select:"_id name"}).exec((error, auctions)=>{
-    if (error) return res.status(400).json({ error });
-    else {
-      res.status(200).json({ auctions });
-    }
-  })
-}
+  const { userId } = req.params;
+  Auction.find({ user: userId, status: 1 })
+    .populate({ path: "product", select: "_id name" })
+    .exec((error, auctions) => {
+      if (error) return res.status(400).json({ error });
+      else {
+        res.status(200).json({ auctions });
+      }
+    });
+};
 
 exports.getAuctionbyUser = (req, res) => {
-  const {userId} = req.params
-  Auction.find({user:userId}).populate({path:"product", select:"_id name currentPrice expiredAt"}).exec((error, auctions)=>{
-    if (error) return res.status(400).json({ error });
-    else {
-      res.status(200).json({ auctions });
-    }
-  })
-}
+  const { userId } = req.params;
+  let products = [];
+  Auction.find({ user: userId })
+    .then(async (auctions) => {
+      for (let auc of auctions) {
+        let pro = await Product.findOne({ _id: auc.product, status: 1 });
+        if (pro) {
+          products.push(pro.name);
+        }
+      }
+    })
+    .then(async() => {
+      let pr = products.filter(
+        (item, index) => products.indexOf(item) === index
+      );
+      let results = [];
+      for (let i = 0; i < pr.length; i++) {
+        let pro2 = await Product.findOne({ name: pr[i] });
+        if(pro2) {
+          results.push(pro2)
+        }
+      }
+      res.status(200).json({results})
+    });
+};
--- src/index.server.js
+++ src/index.server.js
@@ -44,6 +44,8 @@ app.use("/api", addressRoutes);
 app.use("/api", orderRoutes);
 
 
+
+
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
