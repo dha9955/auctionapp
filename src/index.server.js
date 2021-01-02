5cc69c245ecdf3f0195c0835e137cd69d8bf3995
1607dha <1607dha@gmail.com>
2021-01-02 17:13:26 +0700

    updated

--- src/index.server.js
+++ src/index.server.js
@@ -5,12 +5,11 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const nodemailer = require("nodemailer");
-// const socket = require("socket.io");
-// var http = require("http").createServer(app);
-// http.listen(2001);
+const socket = require("socket.io");
+var http = require("http").createServer(app);
+http.listen(2001);
 
-const server = require('http').createServer(app);
-const io = require('socket.io').listen(server)
+
 
 //routes
 const authRoutes = require("./routes/auth");
@@ -51,7 +50,7 @@ app.use("/api", ratingRoutes);
 app.use("/api", addressRoutes);
 app.use("/api", orderRoutes);
 
-// var io = socket(http);
+var io = socket(http);
 
 io.on("connection", (socket) => {
   console.log(socket.id);
@@ -66,6 +65,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(process.env.PORT, () => {
+app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
